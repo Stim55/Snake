953daa2fcf322fa639edae533dd289a5ab87876d
Stim55 <stim5575@yandex.ru>
2018-02-05 00:00:46 +0300

    Функция Draw

--- Snake new/snake/snake/Program.cs
+++ Snake new/snake/snake/Program.cs
@@ -10,10 +10,29 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            int x1 = 5;
+            int y1 = 5;
+            char sym1 = 'O';
+
+            Draw(x1, y1, sym1);
+
+            int x2 = 7;
+            int y2 = 3;
+            char sym2 = '#';
+
+            Draw(x2, y2, sym2);
+
+
             Console.ReadLine();
 
-
         }
-    }
+
+        static void Draw(int x, int y, char sym)
+    
+    {
+     Console.SetCursorPosition(a, b);
+            Console.Write(sym);
+        }
+
+
 }
